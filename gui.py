2564ab08466c4a87dc3986638f4792aaddf30cc6
deut-erium <himanshuthesheoran@gmail.com>
2020-05-22 15:25:28 +0530

    Created class GUI to handle gui and basic structure

--- gui.py
+++ gui.py
@@ -1,28 +1,117 @@
-import tkinter as tk
-from tkinter import *
-window = tk.Tk()
-window.minsize(600,600)
-frm_canvas = tk.Frame(master=window, width=400, height=400, bg="red")
-
-canvas = tk.Canvas(master = frm_canvas,scrollregion=(0,0,1000,1000))
-hbar=tk.Scrollbar(frm_canvas,orient=tk.HORIZONTAL)
-hbar.pack(side=tk.BOTTOM,fill=tk.X)
-hbar.config(command=canvas.xview)
-vbar=tk.Scrollbar(frm_canvas,orient=tk.VERTICAL)
-vbar.pack(side=tk.RIGHT,fill=tk.Y)
-vbar.config(command=canvas.yview)
-canvas.config(xscrollcommand=hbar.set, yscrollcommand=vbar.set)
-canvas.pack(side=tk.LEFT,expand=True,fill=tk.BOTH)
-canvas.create_line(0,0,2000,2000,50,100,200,300)
-canvas.pack()
-frm_parameters = tk.Frame(master=window, width=400,height=200, bg="pink")
-# frm_parameters.grid(row=0,column = 1,sticky = "ES")
-frm_canvas.pack(anchor = "nw",side=tk.TOP,expand=True,fill=tk.BOTH)
-frm_parameters.pack(anchor = "ne",side = tk.BOTTOM,expand = False,fill=tk.BOTH)
+"""Main Class for handling GUI of the application"""
+from tkinter import Tk, Frame, Canvas, Scrollbar, Menu
+from tkinter import HORIZONTAL, VERTICAL, BOTH, TOP, LEFT, RIGHT, X, Y, BOTTOM
 
 
+def todo():
+    """ function substituted to do"""
+    print("OK")
 
 
+class GUI():
+    """Class for handling GUI of application"""
+    def __init__(self, min_height=600, min_width=600):
+        """
+        Initialize the main window of the application
 
-window.mainloop()
+        Initializes other relevant fields too
+        """
+        self.window = Tk()
+        # set minimum size to which the window can be reduced
+        self.window.minsize(min_width, min_height)
+        self.canvas = None
+        self.frm_canvas = None
+        self.frm_parameters = None
+        self.menubar = None
+        self.helpmenu = None
+        self.filemenu = None
+        self.editmenu = None
+        self.init_canvas_frame()
+        self.init_parameters_frame()
+        self.init_menu_bar()
+        self.window.mainloop()
 
+    def init_canvas_frame(self, max_width=1080, max_height=1920):
+        """
+        Creates and initializes a Frame for the canvas
+        max_width, max_height determine the maximum scrollable area
+        """
+        self.frm_canvas = Frame(master=self.window, width=400, height=400)
+        self.canvas = Canvas(
+            master=self.frm_canvas,
+            scrollregion=(0, 0, max_width, max_height))
+        h_scrl_bar = Scrollbar(self.frm_canvas, orient=HORIZONTAL)
+        h_scrl_bar.pack(side=BOTTOM, fill=X)
+        h_scrl_bar.config(command=self.canvas.xview)
+        v_scrl_bar = Scrollbar(self.frm_canvas, orient=VERTICAL)
+        v_scrl_bar.pack(side=RIGHT, fill=Y)
+        v_scrl_bar.config(command=self.canvas.yview)
+        self.canvas.config(
+            xscrollcommand=h_scrl_bar.set,
+            yscrollcommand=v_scrl_bar.set)
+        self.canvas.pack(side=LEFT, expand=True, fill=BOTH)
+        self.frm_canvas.pack(anchor="nw", side=TOP, expand=True, fill=BOTH)
+
+    def init_parameters_frame(self, height=200, width=400):
+        """
+            Initializes frame for parameters/buttons
+        """
+        self.frm_parameters = Frame(
+            master=self.window,
+            width=width,
+            height=height,
+            bg="pink")
+        self.frm_parameters.pack(
+            anchor="ne",
+            side=BOTTOM,
+            expand=False,
+            fill=BOTH)
+
+    def init_menu_bar(self):
+        """
+        initializes the menubar
+        """
+        self.menubar = Menu(self.window)
+        self.init_filemenu()
+        self.init_editmenu()
+        self.init_helpmenu()
+        self.window.config(menu=self.menubar)
+
+    def init_filemenu(self):
+        """
+        initializes filemenu in menubar
+        """
+        self.filemenu = Menu(self.menubar, tearoff=0)
+        self.filemenu.add_command(label="New", command=todo)
+        self.filemenu.add_command(label="Open", command=todo)
+        self.filemenu.add_command(label="Save", command=todo)
+        self.filemenu.add_command(label="Save as...", command=todo)
+        self.filemenu.add_command(label="Close", command=todo)
+        self.filemenu.add_separator()
+        self.menubar.add_cascade(label="File", menu=self.filemenu)
+
+    def init_editmenu(self):
+        """
+        initializes editmenu in menubar
+        """
+        self.editmenu = Menu(self.menubar, tearoff=0)
+        self.editmenu.add_command(label="Undo", command=todo)
+        self.editmenu.add_separator()
+        self.editmenu.add_command(label="Cut", command=todo)
+        self.editmenu.add_command(label="Copy", command=todo)
+        self.editmenu.add_command(label="Paste", command=todo)
+        self.editmenu.add_command(label="Delete", command=todo)
+        self.editmenu.add_command(label="Select All", command=todo)
+        self.menubar.add_cascade(label="Edit", menu=self.editmenu)
+
+    def init_helpmenu(self):
+        """
+        initializes helpmenu in menubar
+        """
+        self.helpmenu = Menu(self.menubar, tearoff=0)
+        self.helpmenu.add_command(label="Help Index", command=todo)
+        self.helpmenu.add_command(label="About...", command=todo)
+        self.menubar.add_cascade(label="Help", menu=self.helpmenu)
+
+
+A_ADV = GUI()
